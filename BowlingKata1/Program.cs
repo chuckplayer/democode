94ed3420e45548edbb777e451800114efe9048ca
chuckplayer <chuck.player@gmail.com>
2014-01-17 21:36:16 -0500

    Setting up

---
+++ BowlingKata1/Program.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Foo">//   
+// </copyright>
+// <summary>
+//   Defines the Program type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BowlingKata1
+{
+    /// <summary>
+    /// The program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ BowlingKata1Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UnitTest1.cs" company="Foo">
+//   
+// </copyright>
+// <summary>
+//   Defines the UnitTest1 type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BowlingKata1Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// The unit test 1.
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// The test method 1.
+        /// </summary>
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
